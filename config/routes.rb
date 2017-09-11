74dd70a13d84de32c0165a4de4f8fbdc459a1ee8
Jared Gallardo <jaredgg@gmail.com>
2017-09-11 14:56:05 -0600

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :items, except: [:new, :edit]
   end
 
+  root
+
   get '*other', to: 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
